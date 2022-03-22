d08ed2526ef862601a3dbc9d94c1b3bde94073cf
Alaaeddine-Chakroun <chakrounalaaeddine@gmail.com>
2022-03-22 12:44:52 +0100

    fix: createAsyncThunk type in CRA

---
+++ apps/cra/declaration.d.ts
@@ -0,0 +1,24 @@
+import {
+  AsyncThunk,
+  AsyncThunkOptions,
+  AsyncThunkPayloadCreator,
+} from '@reduxjs/toolkit';
+
+import { AppDispatch, RootState } from '_redux/store';
+
+type CustomThunkApiConfig = {
+  dispatch: AppDispatch;
+  state: RootState;
+};
+
+declare module '@reduxjs/toolkit' {
+  export declare function createAsyncThunk<Returned, ThunkArg>(
+    typePrefix: string,
+    payloadCreator: AsyncThunkPayloadCreator<
+      Returned,
+      ThunkArg,
+      CustomThunkApiConfig
+    >,
+    options?: AsyncThunkOptions<ThunkArg, CustomThunkApiConfig>
+  ): AsyncThunk<Returned, ThunkArg, CustomThunkApiConfig>;
+}
