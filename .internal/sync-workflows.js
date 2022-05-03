24f3a5b1e65a5cbac9d3777d3f77f90bed22801a
Nader Zouaoui <nader.zouaoui@edonec.com>
2022-05-03 20:58:26 +0100

    feat(eventing): add github workflows

--- .internal/sync-workflows.js
+++ .internal/sync-workflows.js
@@ -3,26 +3,27 @@ const fs = require("fs");
 const path = require("path");
 const mainPackage = require("../package.json");
 
-
 const syncWorkflowsFromBase = (basePath, packages, workflowType) => {
-  const baseWorkflows = fs.readFileSync(path.join(__dirname, basePath)).toString();
-  packages.forEach(package => {
+  const baseWorkflows = fs
+    .readFileSync(path.join(__dirname, basePath))
+    .toString();
+  packages.forEach((package) => {
     console.log(package);
     const workflowFile = `../.github/workflows/${package.packageName}-${workflowType}.yml`;
 
     const newWorkflow = baseWorkflows
       .replace(/\${{packageName}}/g, package.packageName)
       .replace(/\${{packagePath}}/g, package.packagePath)
-      .replace(/\${{isPackage}}/g, package.isApp ? '' : '#');
-    fs.writeFileSync(path.join(__dirname,workflowFile), newWorkflow);
-  }
-  )
-}
+      .replace(/\${{isPackage}}/g, package.isApp ? "" : "#");
+    fs.writeFileSync(path.join(__dirname, workflowFile), newWorkflow);
+  });
+};
 
 const workspaces = mainPackage.workspaces;
 
-const packagesFolder = workspaces
-  .map((workspace) => path.join(__dirname, "../", workspace).replace("*", ""));
+const packagesFolder = workspaces.map((workspace) =>
+  path.join(__dirname, "../", workspace).replace("*", "")
+);
 
 const _overlappingNodePackageFolders = [];
 
@@ -80,36 +81,54 @@ const packageJsonFileFlatUnique = packageJsonFileFlat.filter(
 );
 
 const allJSONFiles = [...packageJsonFileFlatUnique];
-const allPackagesWithTestScripts = []
+const allPackagesWithTestScripts = [];
 
 // TODO: clean up the requires in those folders
 allJSONFiles.forEach((jsonPath) => {
   const buffer = fs.readFileSync(jsonPath);
   const jsonString = buffer.toString();
-  const packageJson = JSON.parse(jsonString)
+  const packageJson = JSON.parse(jsonString);
   if (packageJson.scripts?.test)
-    allPackagesWithTestScripts.push({ name: packageJson.name, jsonPath })
+    allPackagesWithTestScripts.push({ name: packageJson.name, jsonPath });
 });
 
-
-const appsWithTestScript = allPackagesWithTestScripts.filter(package => package.jsonPath.includes('apps'))
-  .map(package => ({
+allPackagesWithTestScripts.forEach((package) => {
+  package.jsonPath = package.jsonPath.replace(/\\/g, "/");
+});
+const appsWithTestScript = allPackagesWithTestScripts
+  .filter((package) => package.jsonPath.includes("apps"))
+  .map((package) => ({
     packageName: package.name,
-    packagePath: `apps/${package.jsonPath.split('/apps/')[1].replace('/package.json', '/**')}`,
-    isApp: true
-  }))
-const packagesWithTestScripts = allPackagesWithTestScripts.filter(package => !package.jsonPath.includes('apps'))
-  .map(package => ({
+    packagePath: `apps/${package.jsonPath
+      .split("/apps/")[1]
+      .replace("/package.json", "/**")}`,
+    isApp: true,
+  }));
+const packagesWithTestScripts = allPackagesWithTestScripts
+  .filter((package) => !package.jsonPath.includes("apps"))
+  .map((package) => ({
     packageName: package.name,
-    packagePath: `packages/${package.jsonPath.split('/packages/')[1].replace('/package.json', '/**')}`,
-    isApp: false
-  }))
-
+    packagePath: `packages/${package.jsonPath
+      .split("/packages/")[1]
+      .replace("/package.json", "/**")}`,
+    isApp: false,
+  }));
 
 // Sync workflows for testing when PR is opened
-syncWorkflowsFromBase('../.github/base.test.yml', [...packagesWithTestScripts, ...appsWithTestScript], 'test')
-syncWorkflowsFromBase('../.github/base.build.yml', [...appsWithTestScript, {
-  packageName: 'proxy-balancer',
-  packagePath: 'proxy-balancer.conf',
-  isApp: false
-}], 'build')
+syncWorkflowsFromBase(
+  "../.github/base.test.yml",
+  [...packagesWithTestScripts, ...appsWithTestScript],
+  "test"
+);
+syncWorkflowsFromBase(
+  "../.github/base.build.yml",
+  [
+    ...appsWithTestScript,
+    {
+      packageName: "proxy-balancer",
+      packagePath: "proxy-balancer.conf",
+      isApp: false,
+    },
+  ],
+  "build"
+);
