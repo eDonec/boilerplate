687f9b3e2880302a818134952b3c76a381a5d65b
Alaaeddine-Chakroun <chakrounalaaeddine@gmail.com>
2022-05-26 12:31:30 +0100

    refactor: ControlledDataTable uses context instead of prop drilling

--- apps/dashboard/src/components/ControlledDataTable/DataTableHeader.tsx
+++ apps/dashboard/src/components/ControlledDataTable/DataTableHeader.tsx
@@ -2,59 +2,59 @@ import { clsx } from "core-utils";
 
 import Chevron from "components/Icons/Chevron";
 
+import { useDataTableContext } from "./DataTableContext";
 import { DataTableHeaderProps, SortDirection } from "./types";
 
 const DEFAULT_SORT_DIRECTION = SortDirection.ASC;
 
-const DataTableHeader = ({
-  headerItems,
-  className,
-  selectedSort,
-  onSortChange,
-}: DataTableHeaderProps) => (
-  <tr
-    className={clsx(
-      className,
-      "bg-gray-50 text-left text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400"
-    )}
-  >
-    {headerItems.map((headerItem) => {
-      const currentSortDirection =
-        selectedSort?.field === headerItem.value
-          ? selectedSort.direction
-          : undefined;
+const DataTableHeader = ({ headerItems }: DataTableHeaderProps) => {
+  const { currentSort, headerClassName, onSortChange } = useDataTableContext();
 
-      return (
-        <th
-          key={headerItem.value}
-          className={clsx(headerItem.className, "cursor-pointer px-6 py-3")}
-          onClick={() =>
-            onSortChange?.({
-              field: headerItem.value,
-              direction:
-                selectedSort?.field === headerItem.value
-                  ? SortDirection[
-                      selectedSort.direction === SortDirection.ASC
-                        ? "DSC"
-                        : "ASC"
-                    ]
-                  : DEFAULT_SORT_DIRECTION,
-            })
-          }
-        >
-          <div className="flex items-center justify-between ">
-            {headerItem.label}
-            <Chevron
-              className={clsx("ml-3 transition-transform", {
-                "rotate-180": currentSortDirection === SortDirection.ASC,
-                invisible: !currentSortDirection,
-              })}
-            />
-          </div>
-        </th>
-      );
-    })}
-  </tr>
-);
+  return (
+    <tr
+      className={clsx(
+        headerClassName,
+        "bg-gray-50 text-left text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400"
+      )}
+    >
+      {headerItems.map((headerItem) => {
+        const currentSortDirection =
+          currentSort?.field === headerItem.value
+            ? currentSort.direction
+            : undefined;
+
+        return (
+          <th
+            key={headerItem.value}
+            className={clsx(headerItem.className, "cursor-pointer px-6 py-3")}
+            onClick={() =>
+              onSortChange?.({
+                field: headerItem.value,
+                direction:
+                  currentSort?.field === headerItem.value
+                    ? SortDirection[
+                        currentSort.direction === SortDirection.ASC
+                          ? "DSC"
+                          : "ASC"
+                      ]
+                    : DEFAULT_SORT_DIRECTION,
+              })
+            }
+          >
+            <div className="flex items-center justify-between ">
+              {headerItem.label}
+              <Chevron
+                className={clsx("ml-3 transition-transform", {
+                  "rotate-180": currentSortDirection === SortDirection.ASC,
+                  invisible: !currentSortDirection,
+                })}
+              />
+            </div>
+          </th>
+        );
+      })}
+    </tr>
+  );
+};
 
 export default DataTableHeader;
--- apps/dashboard/src/components/ControlledDataTable/DataTableRow.tsx
+++ apps/dashboard/src/components/ControlledDataTable/DataTableRow.tsx
@@ -1,24 +1,32 @@
 import { clsx, get } from "core-utils";
 
+import { useDataTableContext } from "./DataTableContext";
 import { DataTableRowProps } from "./types";
 
-const DataTableRow = <T,>({
-  item,
-  columns,
-  className,
-}: DataTableRowProps<T>) => (
-  <tr
-    className={clsx(
-      className,
-      "bg-white dark:border-gray-700 dark:bg-gray-800"
-    )}
-  >
-    {columns.map((col) => (
-      <td className={clsx(col.className, "px-6 py-4")} key={col.selector}>
-        {col.cell ? col.cell(item) : get(item, col.selector)}
-      </td>
-    ))}
-  </tr>
-);
+const DataTableRow = <T,>({ item, index }: DataTableRowProps<T>) => {
+  const {
+    columns,
+    rowClassName,
+    data: { items },
+  } = useDataTableContext<T>();
+
+  return (
+    <tr
+      className={clsx(
+        rowClassName,
+        "bg-white dark:border-gray-700 dark:bg-gray-800",
+        {
+          "border-b": index < items.length,
+        }
+      )}
+    >
+      {columns.map((col) => (
+        <td className={clsx(col.className, "px-6 py-4")} key={col.selector}>
+          {col.cell ? col.cell(item) : get(item, col.selector)}
+        </td>
+      ))}
+    </tr>
+  );
+};
 
 export default DataTableRow;
