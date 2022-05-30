a652cf6fb478826265071c3893e45d01f999aeb5
Nader Zouaoui <nader.zouaoui@edonec.com>
2022-05-30 15:00:02 +0100

    fix(APIs/auth): update test setups

--- apps/APIs/auth/src/routes/authN/__test__/index.test.ts
+++ apps/APIs/auth/src/routes/authN/__test__/index.test.ts
@@ -1,18 +1,15 @@
-/* eslint-disable no-console */
-// import app from "init";
-import app from "init";
+import app from "init.testSetup";
 import { seed } from "seed/seed";
 import { StatusCodes } from "shared-types";
 import supertest from "supertest";
 
-import authNRoutes from "..";
-
-app.use(authNRoutes);
+const BASE_URL = "/api/v1/auth";
 
 beforeEach(async () => {
   try {
     await seed();
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error("Seeding is not available");
   }
 });
@@ -21,34 +18,42 @@ describe("POST /n/classic", () => {
   describe("validation tests", () => {
     it("should respond successfully to email and password and assign default role to PUBLIC", async () => {
       const body = { email: "test@example.com", password: "password" };
-      const response = await supertest(app).post("/n/classic").send(body);
-
-      console.log(response.body);
+      const response = await supertest(app)
+        .post(`${BASE_URL}/n/classic`)
+        .send(body);
 
       expect(response.status).toEqual(StatusCodes.Created);
       expect(response.body.role.name).toEqual("PUBLIC");
     });
     it("should throw a validation error if email is badly formatted", async () => {
       const body = { email: "testexample.com", password: "password" };
-      const response = await supertest(app).post("/n/classic").send(body);
+      const response = await supertest(app)
+        .post(`${BASE_URL}/n/classic`)
+        .send(body);
 
       expect(response.status).toEqual(StatusCodes["Bad Request"]);
     });
     it("should throw a validation error if password is badly formatted", async () => {
       const body = { email: "testexample.com", password: "123" };
-      const response = await supertest(app).post("/n/classic").send(body);
+      const response = await supertest(app)
+        .post(`${BASE_URL}/n/classic`)
+        .send(body);
 
       expect(response.status).toEqual(StatusCodes["Bad Request"]);
     });
     it("should throw a validation error if password field is absent", async () => {
       const body = { email: "testexample.com" };
-      const response = await supertest(app).post("/n/classic").send(body);
+      const response = await supertest(app)
+        .post(`${BASE_URL}/n/classic`)
+        .send(body);
 
       expect(response.status).toEqual(StatusCodes["Bad Request"]);
     });
     it("should throw a validation error if email field is absent", async () => {
       const body = { password: "password" };
-      const response = await supertest(app).post("/n/classic").send(body);
+      const response = await supertest(app)
+        .post(`${BASE_URL}/n/classic`)
+        .send(body);
 
       expect(response.status).toEqual(StatusCodes["Bad Request"]);
     });
@@ -59,11 +64,11 @@ describe("GET /n/me", () => {
   it("should sign-up and receive a token to be used for getting self", async () => {
     const signUpBody = { email: "test1@example.com", password: "password" };
     const signUpResponse = await supertest(app)
-      .post("/n/classic")
+      .post(`${BASE_URL}/n/classic`)
       .send(signUpBody);
 
     const getMeResponse = await supertest(app)
-      .get("/n/me")
+      .get(`${BASE_URL}/n/me`)
       .set("Authorization", `Bearer ${signUpResponse.body.token.accessToken}`);
 
     expect(getMeResponse.body.authID).toBe(signUpResponse.body.authID);
