ad47e339c986420207144108c962d95e647a59c9
Nader Zouaoui <nader.zouaoui@edonec.com>
2022-06-03 00:50:04 +0100

    fix(data-table): type issue with dataTableRow

--- packages/browser/data-table/ControlledDataTable/utils/index.ts
+++ packages/browser/data-table/ControlledDataTable/utils/index.ts
@@ -7,7 +7,7 @@ export const getDataTableHeaderItems = <T>(
 ): HeaderItem[] =>
   columns.map((col) => ({
     label: col.title,
-    value: col.sortCallbackValue || col.selector,
+    value: col.sortCallbackValue || col.selector || "",
     className: col.className,
     sortable: typeof col.sortable === "boolean" ? col.sortable : true,
   }));
