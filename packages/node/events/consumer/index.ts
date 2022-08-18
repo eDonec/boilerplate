ab39ed35172c4e414b784e1b9cfe9c8e2ce8c8c5
Nader Zouaoui <nader.zouaoui@edonec.com>
2022-08-18 13:49:33 +0100

    fix(consumer): fix consumer function logic

--- packages/node/events/consumer/index.ts
+++ packages/node/events/consumer/index.ts
@@ -1,11 +1,18 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable no-console */
 import { Consumer as KafkaConsumer, Kafka, KafkaConfig } from "kafkajs";
 
 class Consumer<E extends { [eventName: string]: string }> {
   private kafka: Kafka;
 
   private consumer: KafkaConsumer;
+
+  private listeners: Record<
+    keyof E,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    undefined | ((message: any, key: any) => void)
+  >;
+
+  private isRunning = false;
 
   private isInitialised = false;
 
@@ -19,8 +26,11 @@ class Consumer<E extends { [eventName: string]: string }> {
     if (!process.env.MICROSERVICE_NAME)
       throw new Error("Missing .env key : MICROSERVICE_NAME");
     this.events = possibleEvents;
-    console.log("initializing Kafka consumer connection");
-
+    // @ts-expect-error this is just an initilizer the real value is assigned on the go
+    this.listeners = {};
+    Object.keys(possibleEvents).forEach((event: keyof E) => {
+      this.listeners[event] = undefined;
+    });
     this.topic = topic;
     this.kafka = new Kafka({
       ...(kafkaConfig || {}),
@@ -28,7 +38,7 @@ class Consumer<E extends { [eventName: string]: string }> {
       brokers: [process.env.KAFKA_BROKERS!],
     });
     this.consumer = this.kafka.consumer({
-      groupId: process.env.MICROSERVICE_NAME!,
+      groupId: `${process.env.MICROSERVICE_NAME!}-${topic}`,
       allowAutoTopicCreation: true,
       heartbeatInterval: 5000,
       retry: {
@@ -53,6 +63,19 @@ class Consumer<E extends { [eventName: string]: string }> {
     });
   }
 
+  private async messageHandler<T, K extends string>(
+    topic: string,
+    message: T,
+    key?: K
+  ) {
+    const eventName = topic.split("-")[1] as keyof E;
+    const listener = this.listeners[eventName];
+
+    if (!listener) return;
+
+    listener(message, key);
+  }
+
   async subscribe<T, K extends string>(
     eventName: keyof E,
     onMessageReceived: (message: T, key?: K) => void
@@ -60,23 +83,29 @@ class Consumer<E extends { [eventName: string]: string }> {
     await this.init();
     if (!this.events[eventName])
       throw new Error(
-        `Event ${eventName} is not defined in the possible events`
+        `Event ${eventName.toString()} is not defined in the possible events`
       );
 
     await this.consumer.subscribe({
-      topic: `${this.topic}-${eventName}`,
+      topic: `${this.topic}-${eventName.toString()}`,
       fromBeginning: true,
     });
 
-    await this.consumer.run({
-      eachMessage: async ({ message }) => {
-        // TODO: update deserializer to follow the same algorithm as serializer
-        onMessageReceived(
-          JSON.parse(message.value?.toString() || "") as T,
-          message.key?.toString() as unknown as K
-        );
-      },
-    });
+    this.listeners[eventName] = onMessageReceived;
+
+    if (!this.isRunning) {
+      this.isRunning = true;
+      await this.consumer.run({
+        eachMessage: async ({ message, topic }) => {
+          // TODO: update deserializer to follow the same algorithm as serializer
+          this.messageHandler(
+            topic,
+            JSON.parse(message.value?.toString() || "") as T,
+            message.key?.toString() as unknown as K
+          );
+        },
+      });
+    }
 
     return this;
   }
@@ -85,11 +114,13 @@ class Consumer<E extends { [eventName: string]: string }> {
     if (this.isInitialised) return this;
     try {
       await this.consumer.connect();
+      // eslint-disable-next-line no-console
       console.log(
         "Kafka consumer connected successfully you can now send messages"
       );
       this.isInitialised = true;
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error("Error connecting to Kafka producer", error);
     }
 
