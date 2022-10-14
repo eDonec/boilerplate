6193133f9eabb2b16f92d8d7897af68464a8a71f
Nader Zouaoui <nader.zouaoui@edonec.com>
2022-10-14 14:55:49 +0100

    fix: generator placeholdere typo

--- generators/api/SDK/index.ts
+++ generators/api/SDK/index.ts
@@ -3,7 +3,7 @@ import ServerSDK from "server-sdk/sdk";
 
 const baseUrl = "/v1/MICROSERVICE_NAME_PLACEHOLDER";
 
-export default class MICROSERVICE_NAME_UPPERCACE_PLACEHOLDERSDK extends ServerSDK {
+export default class MICROSERVICE_NAME_UPPERCASE_PLACEHOLDERSDK extends ServerSDK {
   public async getMicroservicesStatus() {
     const { data } = await this.api.get<
       HealthRouteTypes["/health/"]["GET"]["response"]
--- generators/api/event/consumer.ts
+++ generators/api/event/consumer.ts
@@ -1,29 +1,39 @@
 import Consumer from "consumer";
-import { MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents, MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREventsPayload } from "MICROSERVICE_NAME_PLACEHOLDER-types/events";
+import {
+  MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents,
+  MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREventsPayload,
+} from "MICROSERVICE_NAME_PLACEHOLDER-types/events";
 
-const events = Object.values(MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents);
+const events = Object.values(MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents);
 
-type OnMessageReceivedFunc<EventName extends MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents> = (
-  message: MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREventsPayload[EventName],
+type OnMessageReceivedFunc<
+  EventName extends MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents
+> = (
+  message: MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREventsPayload[EventName],
   key?: EventName
 ) => void;
 
-type TSubscriber<EventName extends MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents> = (
-  onMessageReceived: OnMessageReceivedFunc<EventName>
-) => Promise<void>;
+type TSubscriber<
+  EventName extends MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents
+> = (onMessageReceived: OnMessageReceivedFunc<EventName>) => Promise<void>;
 
 type Subscribers = {
-  [eventName in MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents]: TSubscriber<eventName>;
+  [eventName in MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents]: TSubscriber<eventName>;
 };
-class MICROSERVICE_NAME_UPPERCACE_PLACEHOLDERConsumer {
-  private consumer: Consumer<typeof MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents>;
+class MICROSERVICE_NAME_UPPERCASE_PLACEHOLDERConsumer {
+  private consumer: Consumer<
+    typeof MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents
+  >;
 
   subscribe: Subscribers;
 
   subscribeToAll;
 
   constructor() {
-    this.consumer = new Consumer(MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents, "MICROSERVICE_NAME_PLACEHOLDER");
+    this.consumer = new Consumer(
+      MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents,
+      "MICROSERVICE_NAME_PLACEHOLDER"
+    );
     const sub: Subscribers | Record<string, unknown> = {};
 
     for (let index = 0; index < events.length; index++) {
@@ -37,11 +47,11 @@ class MICROSERVICE_NAME_UPPERCACE_PLACEHOLDERConsumer {
     }
     this.subscribe = sub as Subscribers;
     this.subscribeToAll = (
-      onMessageReceived: OnMessageReceivedFunc<MICROSERVICE_NAME_UPPERCACE_PLACEHOLDEREvents>
+      onMessageReceived: OnMessageReceivedFunc<MICROSERVICE_NAME_UPPERCASE_PLACEHOLDEREvents>
     ) => {
       this.consumer.subscribeToAll(onMessageReceived);
     };
   }
 }
 
-export default MICROSERVICE_NAME_UPPERCACE_PLACEHOLDERConsumer;
+export default MICROSERVICE_NAME_UPPERCASE_PLACEHOLDERConsumer;
