05df56b8e953c99ddf3b2c9b1015bc093e2e7928
Alaaeddine-chakroun <chakrounalaaeddine@gmail.com>
2023-05-11 18:12:27 +0100

    chore: mobile eslint

---
+++ packages/config/config/eslint/eslint-rn.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ...require("./eslint-common.js"),
+  extends: ["@react-native-community"],
+  rules: {
+    ...require("./eslint-common.js").rules,
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "react/jsx-curly-brace-presence": [
+      "warn",
+      { props: "never", children: "never" },
+    ],
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+};
