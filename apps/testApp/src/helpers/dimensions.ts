800c05f486f9b3c4c8ce961c4b115427beacab7e
Alaaeddine-chakroun <chakrounalaaeddine@gmail.com>
2023-05-25 16:14:56 +0100

    chore: remove testApp

--- apps/testApp/src/helpers/dimensions.ts
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
-
-export const vmin = Math.min(height, width) / 100;
-export const vmax = Math.max(height, width) / 100;
--- apps/testApp/src/navigator/Navigator/types.ts
+++
@@ -1,9 +0,0 @@
-import { AppNavigationProp, AppRouteProp } from "navigator/navigatorUtils";
-
-export type NavigatorParamList = {
-  DemoScreen: undefined;
-};
-
-export type NavigatorNavigationProp = AppNavigationProp<NavigatorParamList>;
-export type NavigatorRouteProp<T extends keyof NavigatorParamList> =
-  AppRouteProp<NavigatorParamList, T>;
