3ba5377058def65cedf1262e9bd2627352f97ded
nizar-zouaoui <drekieki244@gmail.com>
2023-06-08 16:39:57 +0100

    fix(Client): reverting branch state

--- apps/client/src/pages/blogs/category/[slug].page.tsx
+++ apps/client/src/pages/blogs/category/[slug].page.tsx
@@ -6,6 +6,7 @@ import { LeanCategoryDocument } from "categories-types/models/Category";
 import { SEO } from "core-next-components";
 import { Button } from "core-ui";
 import { clsx } from "core-utils";
+import { ThreadSorting } from "threads-types/utils";
 
 import { GetStaticPaths, GetStaticProps } from "next";
 import Link from "next/link";
@@ -41,6 +42,16 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         })
       ),
     ]);
+
+    await queryClient.fetchInfiniteQuery(
+      [QueryKeys.THREADS, ThreadSorting.HOT, category.slug],
+      () =>
+        Api.threadsSDK.getThreads({
+          query: {
+            category: category.slug,
+          },
+        })
+    );
 
     return {
       props: {
